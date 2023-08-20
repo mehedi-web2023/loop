2b782b73060d4e0c6144a5e4b32f996e4233e81c
mehedi-web2023 <courseweb38@gmail.com>
2023-08-20 20:05:00 +0600

    loop

---
+++ index.js
@@ -0,0 +1,94 @@
+
+
+
+
+var number = 1;
+
+while(number <= 100){
+    number = number +2
+}
+
+
+
+
+for(var number =0; number <= 100;  number = number +2){
+console.log(number)
+}
+
+
+for(var i =0; i <= 10; i++){
+    console.log("hello")
+}
+
+
+var name =["sanjida","adiba" ,"nus-00","khadiza","mehedi"];
+name[10]
+
+console.log(nume.length)
+var num = 0
+while(num <= name.length){
+    console.log(name[num])
+
+    num++
+}
+
+
+
+var adiba_dilo = 20;
+var sanjida_dilo = 30;
+var nus_00_dilo = 40;
+var khadiza_dilo =50;
+var total = adiba_dilo + sanjida_dilo + nus_00_dilo + khadiza_dilo
+
+
+
+var jira_pani = 25;
+var pani = 15;
+var total_khoroch = jira_pani + pani
+
+var roilo = total - total_khoroch
+
+var distribution = roilo / 4
+console.log(total , total_khoroch , roilo , distribution)
+
+
+
+
+var sanjida = 18;
+var khadija = 20;
+var nusrat = 22;
+
+
+
+
+
+
+if(sanjida==khadija){
+    console.log("friend")
+
+}
+else{
+    console.log("sister")
+
+}
+if( khadija > sanjida ){
+    console.log("hello")
+    if(nusrat>khadija){
+        console.log("Boro Nusrat")
+        
+
+    }
+}
+console.log("nusrat")
+
+
+var name ={
+    firstName:"mehedi",
+    lastname: "hasan",
+    age:25,    
+}
+
+
+
+
+
